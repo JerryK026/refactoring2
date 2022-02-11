654baaba85bc6362fc494b526e8fd2d60b24a40b
SeokyungKim <kskyung0624@gmail.com>
2022-02-11 22:11:11 +0900

    테스트 종료

--- chapter4/app.spec.js
+++ chapter4/app.spec.js
@@ -21,4 +21,58 @@ describe("province", function () {
     expect(asia.shortfall).equal(-6);
     expect(asia.profit).equal(292);
   });
+
+  it("zero demand", function () {
+    asia.demand = 0;
+    expect(asia.shortfall).equal(-25);
+    expect(asia.profit).equal(0);
+  });
+
+  it("negative demand", function () {
+    asia.demand = -1;
+    expect(asia.shortfall).equal(-26);
+    expect(asia.profit).equal(-10);
+  });
+
+  it("empty string demand", function () {
+    asia.demand = "";
+    expect(asia.shortfall).NaN;
+    expect(asia.profit).NaN;
+  });
 });
+
+describe("no producers", function () {
+  // 생산자가 없다.
+  let noProducers;
+  this.beforeEach(function () {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  it("shortfall", function () {
+    expect(noProducers.shortfall).equal(30);
+  });
+  it("profit", function () {
+    expect(noProducers.profit).equal(0);
+  });
+});
+
+// 이런 코드는 작성하지 않는게 좋다. 겉보기 동작에 영향을 주기 때문이다.
+/*
+describe("string for producers", function () {
+  it("", function () {
+    const data = {
+      name: "String producers",
+      producers: "",
+      demand: 30,
+      prce: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).equal(0);
+  });
+});
+*/
