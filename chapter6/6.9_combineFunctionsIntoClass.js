09b3a564388569cc35f7b9315cc491c8e6b4b77e
SeokyungKim <kskyung0624@gmail.com>
2022-02-16 20:55:37 +0900

    여러 함수를 클래스로 묶기

--- chapter6/6.9_combineFunctionsIntoClass.js
+++ chapter6/6.9_combineFunctionsIntoClass.js
@@ -2,17 +2,48 @@ reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 // client1
 {
-  const aReading = acquireReading();
-  const baseCharge =
-    baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading);
+  const baseCharge = aReading.baseCharge;
 }
 
 // client2
 {
-  const aReading = acquireReading();
-  const baseCharge = calculateBaseCharge(aReading);
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading);
+  const taxableCharge = aReading.taxableCharge;
+}
 
-  function calculateBaseCharge(aReading) {
-    baseRate(aReading.month, aReading.year) * aReading.quantity;
+// client3
+{
+  const rawReading = acquireReading();
+  const aReading = new Reading(rawReading);
+  const basicChargeAmount = aReading.baseCharge;
+}
+
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity;
+  }
+  get taxableCharge() {
+    return Math.max(0, this.baseCharge - taxThreshold(this.year));
   }
 }
