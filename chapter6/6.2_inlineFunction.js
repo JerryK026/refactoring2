5e2c6dcab0b0af45403292446da7166bfe99943d
SeokyungKim <kskyung0624@gmail.com>
2022-02-16 20:53:36 +0900

    함수 인라인하기

--- chapter6/6.2_inlineFunction.js
+++ chapter6/6.2_inlineFunction.js
@@ -1,20 +1,13 @@
 function rating(aDriver) {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
 /* ------------------------------------------------ */
 
 function reportLines(aCustomer) {
   const lines = [];
+  lines.push(['name', aCustomer.name]);
+  lines.push(['location', aCustomer.location]);
   gatherCustomerData(lines, aCustomer);
   return lines;
-
-  function gatherCustomerData(out, aCustomer) {
-    out.push(['name', aCustomer.name]);
-    out.push(['location', aCustomer.location]);
-  }
 }
