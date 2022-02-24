e4c922e2f25352da78cb60a8d016eba3f8392363
SeokyungKim <kskyung0624@gmail.com>
2022-02-25 08:08:51 +0900

    컬렉션 캡슐화하기

--- chapter7/7.2_encapsulate_collection.js
+++ chapter7/7.2_encapsulate_collection.js
@@ -3,14 +3,34 @@ class Person {
     this._name = name;
     this._courses = [];
   }
+
   get name() {
     return this._name;
   }
+
+  // 이 메서드들을 사용하지 않고서는 아무도 목록을 변경할 수 없게 만드는 방식
   get courses() {
-    return this._courses;
+    return this._courses.slice();
   }
+
   set courses(aList) {
     this._courses = aList;
+  }
+
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+
+  // 제거 메서드에서는 원소를 제거할 때 대응 방식을 정해야 한다. 무시하거나 에러를 던질 수도 있다.
+  removeCourse(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    }
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
   }
 }
 
@@ -30,7 +50,9 @@ class Course {
 const aPerson = new Person('Jon Do');
 numAdvancedCourses = aPerson.courses.filter((c) => c.isAdvanced).length;
 
+// client1
 {
-  const basicCourseNames = readBasicCourseNames(filename);
-  aPerson.courses = basicCourseNames.map((name) => new Course(name, false));
+  for (const name of readBasicCourseNames(filename)) {
+    aPerson.addCourse(new Course(name, false));
+  }
 }
