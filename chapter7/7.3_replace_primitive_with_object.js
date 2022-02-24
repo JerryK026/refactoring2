9158ffb11e72ee719755797e86271a3f76a9a52e
SeokyungKim <kskyung0624@gmail.com>
2022-02-25 08:32:06 +0900

    기본형을 객체로 만들기

--- chapter7/7.3_replace_primitive_with_object.js
+++ chapter7/7.3_replace_primitive_with_object.js
@@ -3,14 +3,57 @@ class Order {
     this.priority = data.priority;
     // 나머지 초화 코드 생략
   }
+
+  get priority() {
+    return this._priority;
+  }
+
+  get priorityString() {
+    return this._priority.toString();
+  }
+
+  set priority(aString) {
+    this._priority = new Priority(aString);
+  }
+}
+
+// Order에서 받아오는 priority를 아예 클래스로 추상화 해 얻어올 수 있게 한다.
+class Priority {
+  constructor(value) {
+    if (value instanceof Priority) return value;
+    if (Priority.legalValues().includes(value)) this._value = value;
+    else throw new Error(`<${value}>는 유효하지 않은 우선순위입니다.`);
+  }
+
+  toString() {
+    return this._value;
+  }
+
+  get _index() {
+    return Priority.legalValues().findIndex((s) => s === this._value);
+  }
+
+  static legalValues() {
+    return ["low", "normal", "high", "rush"];
+  }
+
+  equals(other) {
+    return this._index === other._index;
+  }
+  higherThan(other) {
+    return this._index > other._index;
+  }
+  lowerThan(other) {
+    return this._index < other._index;
+  }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order({ priority: "normal" }),
+  new Order({ priority: "high" }),
+  new Order({ priority: "rush" }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
+const highPriorityCount = orders.filter((o) =>
+  o.priority.higherThan(new Priority("normal"))
 ).length;
