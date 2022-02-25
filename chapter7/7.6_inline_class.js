43ac2b4f2d979954baac978db3d2876e75e52077
SeokyungKim <kskyung0624@gmail.com>
2022-02-25 13:27:04 +0900

    클래스 인라인하기

--- chapter7/7.6_inline_class.js
+++ chapter7/7.6_inline_class.js
@@ -1,29 +1,33 @@
-class TrackingInformation {
-  get shippingCompany() {
-    return this._shippingCompany;
-  }
-  set shippingCompany(arg) {
-    this._shippingCompany = arg;
-  }
-  get trackingNumber() {
-    return this._trackingNumber;
-  }
-  set trackingNumber(arg) {
-    this._trackingNumber = arg;
-  }
-  get display() {
-    return `${this.shippingCompany}: ${this.trackingNumber}`;
-  }
-}
-
 class Shipment {
   get trackingInfo() {
-    return this._trackingInformation.display;
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
+
   get trackingInformation() {
     return this._trackingInformation;
   }
+
   set trackingInformation(aTrackingInformation) {
     this._trackingInformation = aTrackingInformation;
   }
+
+  set shippingCompany(arg) {
+    this._shippingCompany = arg;
+  }
+
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
 }
+
+const aShipment = new Shipment();
+aShipment.shippingCompany = request.vendor;
+aShipment.trackingNumber = request.number;
