bcbdeed12f78bbea6e6c263fc1af00b0572904ae
SeokyungKim <kskyung0624@gmail.com>
2022-02-28 21:04:35 +0900

    8.1 함수 옮기기 - 예제2 추가

---
+++ chapter8/8.1_moveFunction2.js
@@ -0,0 +1,16 @@
+class Account {
+  // 은행 이자 계산
+  get bankCharge() {
+    let result = 4.5;
+    if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+    return result;
+  }
+
+  get overdraftCharge() {
+    if (this.type.isPremium) {
+      const baseCharge = 10;
+      if (this.daysOverdrawn <= 7) return baseCharge;
+      else return baseCharge + (this.daysOverdrawn - 7) * 0.05;
+    } else return this.daysOverdrawn * 1.75;
+  }
+}
