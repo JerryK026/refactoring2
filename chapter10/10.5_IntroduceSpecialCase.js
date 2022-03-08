6ec47f33befa0298d814d2f4f7ac9a5b4faebc14
SeokyungKim <kskyung0624@gmail.com>
2022-03-08 23:32:05 +0900

    10.5 특이 케이스 추가하기 - 예제 1 수정

--- chapter10/10.5_IntroduceSpecialCase.js
+++ chapter10/10.5_IntroduceSpecialCase.js
@@ -11,26 +11,35 @@ class Customer {
   get paymentHistory() {} // 납부 이력
 }
 
-{
-  const site = new Site();
+const site = new Site();
 
-  // client 1
-  const aCustomer = site.customer;
-  // ...수많은 코드...
+const aCustomer = site.customer;
+
+// ...수많은 코드...
+
+// client 1
+{
   let customerName;
   if (aCustomer === "미확인 고객") customerName = "거주자";
   else customerName = aCustomer.name;
+}
 
-  // client2
+// client2
+{
   const plan =
     aCustomer === "미확인 고객"
-      ? CustomElementRegistry.billingPlans.basic
+      ? registry.billingPlans.basic
       : aCustomer.billingPlan;
+}
 
-  // client3
+// client3
+
+{
   if (aCustomer !== "미확인 고객") aCustomer.billingPlan = newPlan;
+}
 
-  // client4
+// client4
+{
   const weeksDelinquent =
     aCustomer === "미확인 고객"
       ? 0
