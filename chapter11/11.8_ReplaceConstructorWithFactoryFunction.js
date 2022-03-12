b0c187a566f268fc3bf87b76b08b5d99aa385ea5
SeokyungKim <kskyung0624@gmail.com>
2022-03-12 23:11:44 +0900

    11.8 생성자를 팩터리 함수로 바꾸기 - 예제 1 추가

---
+++ chapter11/11.8_ReplaceConstructorWithFactoryFunction.js
@@ -0,0 +1,19 @@
+class Employee {
+  constructor(name, typeCode) {
+    this._name = name;
+    this._typeCode = typeCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get type() {
+    return Employee.legalTypeCodes[this._typeCode];
+  }
+  static get legalTypeCodes() {
+    return { E: "Engineer", M: "Manager", S: "SalePerson" };
+  }
+}
+
+// 호출자
+candidate = new Employee(document.name, document.empType);
