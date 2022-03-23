cad410cdde9415b0f69b2830dfb533277d108200
SeokyungKim <kskyung0624@gmail.com>
2022-03-24 00:37:14 +0900

    12.10 서브클래스를 위임으로 바꾸기 - 예제 1 추가

---
+++ chapter12/12.10_ReplaceSubclassWithDelegate.js
@@ -0,0 +1,43 @@
+class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+  }
+
+  get hasTalkback() {
+    return this._show.hasOwnProperty("talkback");
+  }
+
+  get basePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return result;
+  }
+}
+
+class PremiumBooking extends Booking {
+  constructor(show, date, extras) {
+    super(show, date);
+    this._extras = extras;
+  }
+
+  get basePrice() {
+    return Math.round(super.basePrice + this._extras.premiumFee);
+  }
+
+  get hasDinner() {
+    return this._extras.hasOwnProperty("dinner") && !this.isPeakDay;
+  }
+}
+
+let aBooking;
+
+// 클라이언트(일반 예약)
+{
+  aBooking = new Booking(show, date);
+}
+
+// 클라이언트(프리미엄 예약)
+{
+  aBooking = new PremiumBooking(show, date, extras);
+}
